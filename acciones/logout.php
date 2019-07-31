7cb872c0baf7ffa08af1e328aea3c5331bf02466
Nide90 <49660925+Nide90@users.noreply.github.com>
2019-07-30 23:43:04 -0300

    Delete logout.php

--- acciones/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-    // Destruyo la sesion para que se borren todos los datos guardados en ella
-    session_destroy();
-
-    // Redirijo a la pagina de login
-    header('Location: APP/login/login.php');
-?>
